b925bc5f8bf2a397269743ddb06395e95c19c9f3
zohashobbar <zoha.shobbar@outlook.com>
2025-04-07 23:57:32 +0330

    add identity entities to domain

---
+++ src/Server/Eventify.Server.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eventify.Server.Domain.Entities.Identity;
+
+public partial class Role : IdentityRole<Guid>
+{
+}
+
+public partial class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.Property(role => role.Name).HasMaxLength(50);
+    }
+}
+
